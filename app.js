076e41c893e96799a8ca4af21c6cec05fbdd82bf
shameemmonir <shameemmonir@gmail.com>
2024-07-01 06:43:49 +0600

    second commit

--- app.js
+++ app.js
@@ -21,17 +21,47 @@ for (const btn of allBtn) {
         const p = document.createElement('p');
         p.innerText = placeName;
         const p2 = document.createElement('p');
-        p.innerText = price;
+        p2.innerText = price;
 
         li.appendChild(p);
         li.appendChild(p2);
         selectedContainer.appendChild(li);
 
 
+        totalCost('total-cost', parseInt(price));
+
+        const totalCost = document.getElementById('total-cost').innerText;
+
+        // console.log(typeof parseInt(totalCost));
+        // const convertedTotalCost = parseInt(totalCost);
+        // document.getElementById('total-cost').innerText = convertedTotalCost + parseInt(price);
+
+        const convertedTotalCost = parseInt(totalCost);
+        const sum = convertedTotalCost + parseInt(price);
+        setInnerText('total-cost', sum);
+
+        const grandTotal = document.getElementById('grand-total').innerText;
+        const convertedGrandTotal = parseInt(grandTotal);
+
+        const sum2 = convertedGrandTotal + parseInt(price);
+
+        setInnerText('grand-total', sum2);
+
+        setInnerText('total-cost', sum);
+
         setInnerText('cart-count', count);
     })
 }
 
-function setInnerText (id, value){
-    document.getElementById(id).innerText= value;
+function totalCost(id, value){
+    const totalCost = document.getElementById(id).innerText;
+    const convertedTotalCost = parseInt(value);
 }
+
+function setInnerText (id, value){
+    document.getElementById(id).innerText;
+    const convertedTotalCost = parseInt(totalCost);
+    const sum = convertedTotalCost + parseInt(value);
+    totalCost('total-cost', sum);
+}
+
