6f4754e8740701ac53f8c9bde8163ba1e0879182
shameemmonir <shameemmonir@gmail.com>
2024-07-02 06:59:00 +0600

    second part completed

--- test.js
+++ test.js
@@ -1,8 +1,41 @@
-// document.getElementById('btn').addEventListener('click', function(){
-//     console.log('hafsa');
-// })
+const allBtn = document.getElementsByClassName('add-btn');
+// console.log(allBtn);
 
-function handleClick (event){
-    console.log(event.target.parentNode.parentNode.childNodes[1].innerText);
+let count = 0;
+
+for (const btn of allBtn) {
+    // console.log(btn);
+    btn.addEventListener('click', function (e) {
+        // console.log('HafsaMoni');
+        count = count + 1;
+
+        const placeName = e.target.parentNode.childNodes[1].innerText;
+
+        const price = e.target.parentNode.childNodes[3].childNodes[1].innerText;
+
+        const selectedContainer = document.getElementById('selected-place-container');
+
+        const li = document.createElement('li');
+
+        const p = document.createElement('p')
+        p.innerText = placeName;
+        const p2 = document.createElement('p');
+        p2.innerText = price;
+
+        li.appendChild(p);
+        li.appendChild(p2);
+
+        selectedContainer.appendChild(li);
+
+        // document.getElementById('cart-count').innerText = count;
+
+        const totalCost = document.getElementById('total-cost').innerText;
+        console.log(typeof totalCost);
+        
+        setInnerText('cart-count', count);
+    })
 }
 
+function setInnerText(id, value) {
+    document.getElementById(id).innerText = value;
+}
